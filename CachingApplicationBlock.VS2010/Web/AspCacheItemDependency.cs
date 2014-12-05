7e49d1d79335be92448192ba029839cbd5f14ca8
王 玉才 <180261899@qq.com>
2014-12-05 14:08:54 +0800

    新增类：AspCacheItemDependency。

---
+++ CachingApplicationBlock.VS2010/Web/AspCacheItemDependency.cs
@@ -0,0 +1,59 @@
+#region README
+
+/*
+ * README
+ * 
+ * Current Thread User : GUOCOLAND\wangyucai
+ * Machine Name : GLCHQWYCWINW7
+ * Visual Studio : Microsoft Visual Studio 2010 Ultimate Edition
+ * Create Time : 2014-12-05 14:01:51
+ * Common Language Runtime : 4.0.30319.18444
+ * Minimum .Net Framework Version : 4.0
+ * 
+ * SourcePro Studio 2014
+ * Project Url : https://github.com/SourceproStudio/CodeTemplates
+ * Home Page Url : https://github.com/SourceproStudio
+ * E-mail Address : MasterDuner@yeah.net or Yucai.Wang-Public@outlook.com
+ * QQ : 180261899
+ */
+
+#endregion
+
+using System;
+using SourcePro.Csharp.Practices.FoundationLibrary.Caching;
+
+namespace SourcePro.Csharp.Practices.FoundationLibrary.Web.Caching
+{
+    /// <summary>
+    /// <para>
+    /// 提供了ASP.NET缓存依赖的方法。
+    /// </para>
+    /// <para>
+    /// Namespace : <see cref="SourcePro.Csharp.Practices.FoundationLibrary.Web.Caching"/>
+    /// </para>
+    /// <para>
+    /// Type : <see cref="AspCacheItemDependency"/>
+    /// </para>
+    /// <para>
+    /// The minimum .Net Framework version requirements : 4.0
+    /// </para>
+    /// </summary>
+    /// <seealso cref="SourcePro.Csharp.Practices.FoundationLibrary.Web.Caching"/>
+    public class AspCacheItemDependency : CacheItemDependency
+    {
+        #region AspCacheItemDependency Constructors
+
+        /// <summary>
+        /// 用于初始化一个<see cref="AspCacheItemDependency" />对象实例。
+        /// </summary>
+        public AspCacheItemDependency(params string[] files)
+            : base(files)
+        { }
+
+        #endregion
+    }
+}
+
+/*
+ * Copyright © 2014 Wang Yucai. All rights reserved.
+ */
