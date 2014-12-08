f469b50131e362c2905ac529e118e24445058aec
王 玉才 <180261899@qq.com>
2014-12-08 16:46:25 +0800

    新增类：ConfigurationStorage。

---
+++ ConfigurationApplicationBlock.VS2010/ConfigurationStorage.cs
@@ -0,0 +1,96 @@
+#region README
+
+/*
+ * README
+ * 
+ * Current Thread User : GUOCOLAND\wangyucai
+ * Machine Name : GLCHQWYCWINW7
+ * Visual Studio : Microsoft Visual Studio 2010 Ultimate Edition
+ * Create Time : 2014-12-08 16:37:40
+ * Common Language Runtime : 4.0.30319.18444
+ * Minimum .Net Framework Version : 4.0
+ * 
+ * SourcePro Studio 2014
+ * Project Url : https://github.com/SourceproStudio/CodeTemplates
+ * Home Page Url : https://github.com/SourceproStudio
+ * E-mail Address : MasterDuner@yeah.net or Yucai.Wang-Public@outlook.com
+ * QQ : 180261899
+ */
+
+#endregion
+
+using System;
+using SourcePro.Csharp.Practices.FoundationLibrary.Caching;
+
+namespace SourcePro.Csharp.Practices.FoundationLibrary.Commons.Configuration
+{
+    /// <summary>
+    /// <para>
+    /// 提供了保存配置信息的方法。
+    /// </para>
+    /// <para>
+    /// Namespace : <see cref="SourcePro.Csharp.Practices.FoundationLibrary.Commons.Configuration"/>
+    /// </para>
+    /// <para>
+    /// Type : <see cref="ConfigurationStorage"/>
+    /// </para>
+    /// <para>
+    /// The minimum .Net Framework version requirements : 4.0
+    /// </para>
+    /// </summary>
+    /// <seealso cref="SourcePro.Csharp.Practices.FoundationLibrary.Commons.Configuration"/>
+    public class ConfigurationStorage
+    {
+        private Cache cache = new Cache();
+
+        #region ConfigurationStorage Constructors
+
+        /// <summary>
+        /// 用于初始化一个<see cref="ConfigurationStorage" />对象实例。
+        /// </summary>
+        public ConfigurationStorage()
+        { }
+
+        #endregion
+
+        #region Save
+        /// <summary>
+        /// 保存到缓存区。
+        /// </summary>
+        /// <param name="sectionName">自定义配置节（组）名称。</param>
+        /// <param name="section">自定义配置节（组）。</param>
+        /// <param name="dependencies">缓存依赖文件。</param>
+        public virtual void Save(string sectionName, object section, params string[] dependencies)
+        {
+            cache.Add(new CacheItem(sectionName, section)
+            {
+                Policy = new CacheItemPolicy()
+                {
+                    AbsoluteExpiration = DateTime.Now.AddDays(1),
+                    SlidingExpiration = CacheItemPolicy.NoneSlidingExpiration,
+                    Dependency = new CacheItemDependency(dependencies),
+                    Priority = CacheItemPriority.Default,
+                    RemovedCallBack = null,
+                    UpdatedCallBack = null
+                }
+            });
+        }
+        #endregion
+
+        #region IsExists
+        /// <summary>
+        /// 验证是否缓存了配置节（组）信息。
+        /// </summary>
+        /// <param name="sectionName">配置节（组）名称。</param>
+        /// <returns>是否已经写入缓存。</returns>
+        public virtual bool IsExists(string sectionName)
+        {
+            return cache.IsExists(sectionName);
+        }
+        #endregion
+    }
+}
+
+/*
+ * Copyright © 2014 Wang Yucai. All rights reserved.
+ */
